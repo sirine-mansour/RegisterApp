f13860463daee3bec6304b79d117b8428c0868e6
sirine mansour <148336396+sirine-mansour@users.noreply.github.com>
2023-11-20 21:12:37 +0100

    button
    affichage

--- js/min.js
+++ js/min.js
@@ -1,2 +1,25 @@
 const nom = document.querySelector('.nom');
 console.log (nom.value);
+const submit = document.querySelector('.submit');
+console.log (submit.value);
+
+function validatenom (nom) {
+    if(!nom) {
+        return " nom is required.";
+    }
+    if (nom.length < 4 ){
+        return "nom must be between 3 and 20 characters long.";
+     }
+       
+        if (!/^[a-zA-Z]+$/.test(nom)) {
+           return "nom must contain only letters and spaces.";
+        }
+        return null;
+     }
+
+submit.addEventListener('click' , (event) => {
+    if ( validatenom(nom.value) !== null) {
+        alert (validatenom (nom.value))
+        return;
+    }   } )
+
